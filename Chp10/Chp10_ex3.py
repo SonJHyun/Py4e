f3bf696c8c4338df9da1e7ba0a2c269c16f5ab96
ninpo <sonjh.code@gmail.com>
2022-10-27 17:34:31 +0900

    Modified ex3 for correct solution

--- Chp10/Chp10_ex3.py
+++ Chp10/Chp10_ex3.py
@@ -1,3 +1,6 @@
+import string
+chars = dict()
+
 fname = input("Please enter a file name: ")
 
 try:
@@ -10,7 +13,20 @@ except:
 for line in fhand:
     line = line.strip()
     line = line.lower()
+    line = line.translate(str.maketrans('','',string.punctuation))
+    line = line.replace(' ', '')
 
-    print(line)
+    for char in line:
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
 
 
+chars_list = list()
+
+for key, val in list(chars.items()):
+    chars_list.append((key, val))
+
+chars_list.sort()
+print(chars_list)
